8ccd1bc798d31cc51a2a6ff5e837276998de6039
delphine abouab <delphine.abouab@outlook.fr>
2023-07-20 16:35:42 +0200

    lazy state for local storage

--- src/App.jsx
+++ src/App.jsx
@@ -6,15 +6,11 @@ import Split from "react-split"
 import {nanoid} from "nanoid"
 
 export default function App() {
-    const [notes, setNotes] = React.useState(
-      (localStorage.getItem("notes") ?
-        JSON.parse(localStorage.getItem("notes"))
-        : []
-      ))
+    const [notes, setNotes] = React.useState(() => JSON.parse(localStorage.getItem("notes")) || [])
 
     const [currentNoteId, setCurrentNoteId] = React.useState(
         (notes[0] && notes[0].id) || ""
-    )
+    )|| []
 
     React.useEffect(() => {
       localStorage.setItem("notes", JSON.stringify(notes))
